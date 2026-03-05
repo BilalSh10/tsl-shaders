199185dcffb212cab571d100596a4bb8008485cc
Bilal <bilal_shweike@hotmail.com>
2026-03-05 14:55:32 +0200

    c

--- app/Experience.tsx
+++ app/Experience.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 const Experience = () => {
   return (
-      <Canvas camera={{fov: 55, near: 0.1, far: 100, position: [2, 2, 2]}}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <OrbitControls />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshNormalMaterial />
-        </mesh>
-     </Canvas>
-  )
-}
+    <Canvas camera={{ fov: 75, near: 0.1, far: 100, position: [2, 2, 3] }}>
+      <color attach="background" args={["#F5EFFF"]} />
+      <OrbitControls />
+      <ambientLight intensity={2} />
+      <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[4, 4, 20, 20]} />
+        <meshStandardMaterial color="#E77728" />
+      </mesh>
+    </Canvas>
+  );
+};
 
-export default Experience
+export default Experience;
